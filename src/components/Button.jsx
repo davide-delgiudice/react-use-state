cc96253e87193ea46c9f97d389fd1184cebf02a6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-08 17:51:40 +0200

    [UPD] add state, arrow function and ternary operator for showing and hide with click - Esercizio Terminato

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import languages from '../assets/languages';
+import { useState } from 'react';
 
 function Button() {
-    const sayHello = () => {
-        alert("Hello!");
-    };
+    const [activeLanguage, setActiveLanguage] = useState(null)
 
   return (
     <div>
         <ul>
         {languages.map((language) => (
             <li key={`language-${language.id}`}>
-                <button onClick={sayHello}>
+                <button onClick={() => setActiveLanguage(activeLanguage === language.id ? null : language.id)}>
                     {language.title}
                 </button>
-                <div className='card'>
-                    <h3>{language.title}</h3>
-                    {language.description}
-                </div>
+                {activeLanguage === language.id ? (
+                    <div className='card'>
+                        <h3>{language.title}</h3>
+                        {language.description}
+                    </div>
+                ) : null }
             </li>
         ))}
         </ul>
