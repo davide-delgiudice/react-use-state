f08ac5927edca4441a638586de442dfd14b6e9a1
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-08 15:21:47 +0200

    [UPD] create placeholder btn function

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 function Main() {
+
+  const sayHello = () => {
+    alert("Hello!");
+  };
+
   return (
     <main>
       <div className='container'>
         <h1>Learn Web development</h1>
+      </div>
+      <div>
+        <button onClick={sayHello}>Click me!</button>
       </div>
     </main>
   )
