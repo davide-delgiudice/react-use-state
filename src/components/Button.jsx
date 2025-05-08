b811de5c495cb70d3f9abd223f82a85f6b5e4dca
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-08 19:35:33 +0200

    [UPD] add different css classes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,11 @@ function Button() {
     const [activeLanguage, setActiveLanguage] = useState(null)
 
   return (
-    <div>
-        <ul>
+    <div className='card-box'>
+        <ul className='d-flex'>
         {languages.map((language) => (
             <li key={`language-${language.id}`}>
-                <button onClick={() => setActiveLanguage(activeLanguage === language.id ? null : language.id)}>
+                <button onClick={() => setActiveLanguage(activeLanguage === language.id ? null : language.id)} className={activeLanguage === language.id ? 'active' : ''}>
                     {language.title}
                 </button>
                 {activeLanguage === language.id ? (
